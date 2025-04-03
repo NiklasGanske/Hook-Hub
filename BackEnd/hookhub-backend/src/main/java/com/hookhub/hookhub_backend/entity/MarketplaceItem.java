b792d8b28c49f6d500ba72cd2adc96535cd3d6c3
NiklasGanske <ganske25@web.de>
2025-04-03 15:48:33 +0200

    feature/ added missing Entities

--- BackEnd/hookhub-backend/src/main/java/com/hookhub/hookhub_backend/entity/MarketplaceItem.java
+++ BackEnd/hookhub-backend/src/main/java/com/hookhub/hookhub_backend/entity/MarketplaceItem.java
@@ -1,5 +1,29 @@
 package com.hookhub.hookhub_backend.entity;
 
+import java.util.concurrent.locks.Condition;
+
+import com.hookhub.hookhub_backend.enums.SaleStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MarketplaceItem {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private Long userId;
+    private Long gearId;
+    private double price;
+    private Condition condition;
+    private SaleStatus status;
 }
--- BackEnd/hookhub-backend/src/main/java/com/hookhub/hookhub_backend/entity/Post.java
+++ BackEnd/hookhub-backend/src/main/java/com/hookhub/hookhub_backend/entity/Post.java
@@ -1,5 +1,25 @@
 package com.hookhub.hookhub_backend.entity;
 
-public class Post {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long userId;
+    private String content;
+    @ManyToOne
+    private Long catchId;
+    private String createdAt;
 }
